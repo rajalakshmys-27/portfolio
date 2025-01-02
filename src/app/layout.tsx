adf3966f0a17b0a817618689fc368a5cc58e356a
rajalakshmys-27 <rajalakshmys27@gmail.com>
2025-01-02 19:08:39 +0530

    speed insights bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
