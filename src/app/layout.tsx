60a2d648f4c50aca7c20cd6e07f60633e96eb834
rajalakshmys-27 <rajalakshmys27@gmail.com>
2025-01-12 16:13:51 +0530

    seo optimisations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,12 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Rajalakshmy | Portfolio",
-  description: "Portfolio website",
+  description: "A showcase of my projects, skills, and experiences.",
   icons: {
     icon: "/assets/favicon.ico"
+  },
+  alternates: {
+    canonical: "https://rajalakshmy-portfolio.vercel.app/"
   }
 };
 
@@ -31,6 +34,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="description" content="Rajalakshmy S's Portfolio - Front-End Developer specializing in React.js and MERN stack" />
+        <meta name="keywords" content="Rajalakshmy S, Front-End Developer, React.js, MERN stack, Portfolio" />
+        <meta name="author" content="Rajalakshmy S" />
+        <meta name="google-site-verification" content="v_ivi5lGMI1EyRNYhkxWnhRwR54y3uj8Z11BbCB0hkc" />
+        <link rel="icon" href="/assets/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
