769b4dcd07f742dcace33d424671ea36f733fa8c
rajalakshmys-27 <rajalakshmys27@gmail.com>
2025-01-23 15:49:30 +0530

    elaboration on experience

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -11,18 +11,21 @@ const Experiences = () => {
     const experiences = [
         {
             year: "2024 - Present",
+            designation: "Systems Engineer",
             description:
-                "Developed full-stack applications using Node.js, Express.js, MongoDB, and GraphQL. Leveraged Next.js for server-side rendering and secure authentication.",
+                "Building full-stack applications with Node.js, Express.js, MongoDB, and GraphQL, integrating secure authentication systems. Using Next.js for server-side rendering to create dynamic, high-performance web applications while ensuring robust access control mechanisms.",
         },
         {
             year: "2023 - 2024",
+            designation: "Assistant Systems Engineer",
             description:
-                "Revamped an airline booking platform using React JS, Redux Toolkit, and SCSS. Integrated Google OAuth 2.0 and multilingual support using i18next.",
+                "Redesigned an airline booking platform using React.js, Redux Toolkit, and SCSS to deliver a modern user interface. Integrated features like Google OAuth 2.0 and multilingual support to enhance the platform's usability and accessibility.",
         },
         {
             year: "2021 - 2023",
+            designation: "Assistant Systems Engineer",
             description:
-                "Built responsive front-end components for an e-commerce platform using React JS and LESS, improving performance by 30%.",
+                "Developed and maintained responsive user interfaces for an e-commerce platform with React.js and LESS. Collaborated on implementing efficient deployment pipelines and ensured consistent cross-browser compatibility for seamless user experiences.",
         },
     ];
 
@@ -31,7 +34,6 @@ const Experiences = () => {
             id="experience"
             className={`md:min-h-screen py-20 ${isLightMode ? "text-black bg-[#ffffff]" : "text-white bg-[#141414]"}`}
         >
-            {/* Heading Section */}
             <motion.h2
                 initial={{ opacity: 0, y: -50 }}
                 animate={{ opacity: 1, y: 0 }}
@@ -41,8 +43,6 @@ const Experiences = () => {
                 Experience
             </motion.h2>
             <div className="max-w-7xl mx-auto px-6 md:px-12 lg:px-18 flex flex-col md:flex-row gap-5 items-center justify-between">
-
-                {/* Left Section - Timeline */}
                 <motion.div
                     initial={{ opacity: 0, x: -100 }}
                     animate={{ opacity: 1, x: 0 }}
@@ -50,7 +50,7 @@ const Experiences = () => {
                     className="basis-3/5 relative"
                 >
                     <h3 className="text-2xl md:text-3xl font-semibold mb-6">
-                        Tata Consultancy Services <br className="lg:hidden"/>(2021 - Present)
+                        Tata Consultancy Services <br className="lg:hidden" />(2021 - Present)
                     </h3>
                     <div className="relative border-l-2 border-purple-400">
                         {experiences.map((exp, index) => (
@@ -64,7 +64,10 @@ const Experiences = () => {
                             >
                                 <div className="absolute -left-2.5 top-0 w-4 h-4 rounded-full bg-purple-400 border-2 border-white"></div>
                                 <span className="block text-lg font-bold text-purple-400 mb-2">
-                                    {exp.year}
+                                    {exp.year} {"| "}
+                                    <span className="text-gray-500 font-semibold">
+                                        {exp.designation}
+                                    </span>
                                 </span>
                                 <p
                                     className={`text-sm sm:text-base leading-relaxed ${isLightMode ? "text-gray-950" : "text-gray-300"}`}
@@ -75,8 +78,6 @@ const Experiences = () => {
                         ))}
                     </div>
                 </motion.div>
-
-                {/* Right Section - Image */}
                 <motion.div
                     initial={{ opacity: 0, x: 50 }}
                     animate={{ opacity: 1, x: 0 }}
