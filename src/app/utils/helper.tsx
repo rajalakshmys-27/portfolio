185c668553f0933a720dc18f05e98b9a8825a5c2
Rajalakshmy S <83861940+rajalakshmys-27@users.noreply.github.com>
2025-08-24 19:28:31 +0530

    Update helper.tsx with getExperience function to dynamically generate years

--- src/app/utils/helper.tsx
+++ src/app/utils/helper.tsx
@@ -1,3 +1,13 @@
 export const copyToClipboard = (text: string) => {
     navigator.clipboard.writeText(text);
 };
+
+export function getExperienceYears() {
+  const startDate = new Date("2021-07-29"); // Joining date
+  const now = new Date();
+
+  const diffInMs = now - startDate;
+  const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365.25); // rough years incl. leap years
+
+  return diffInYears.toFixed(1); // keep 1 decimal, e.g., "3.6"
+}
